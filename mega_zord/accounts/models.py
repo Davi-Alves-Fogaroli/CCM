4d3e87e80966bbc2ef252022d8c89c5c6d7caab8
Davi <davi.a.fogaroli@gmail.com>
2022-05-03 11:10:50 -0300

    reconfiguração do model user

--- mega_zord/accounts/models.py
+++ mega_zord/accounts/models.py
@@ -1,8 +1,4 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-from django.contrib.auth.models import User
-
-class Employee(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    department = models.CharField(max_length=100)
+class User(AbstractUser):
+    pass
